fcb6ada9edcefd234d1884bf2403076797febe7c
B.J. Rossiter <b.j.rossiter@gmail.com>
2012-01-15 16:19:39 +1100

    get around rvm trust issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,3 +92,9 @@ after 'deploy:symlink' do
   deploy.copy_resque_assets
 end
 
+namespace :rvm do
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
+  end
+end
+after "deploy", "rvm:trust_rvmrc"
